f393a18f3604da8e8f671023e6648bb5754b0bae
GehadAbdelmaguid <gehad.ezzeldin96@gmail.com>
2023-04-21 19:22:01 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a <= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
